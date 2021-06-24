faf3f9f923c35075bc03d228e6f98b53380335e5
JonnasWillian <jonnasnogueira2@gmail.com>
2021-06-24 09:23:15 -0300

    'Aula03'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { Route,  BrowserRouter, Switch } from 'react-router-dom'
 
-import { AuthContextProvider } from './contexts/AuthContext'
-
 import { Home } from "./pages/Home";
-import { Room } from './pages/Room';
 import { NewRoom } from "./pages/NewRoom";
-import { AdminRoom } from './pages/AdminRoom';
+
+import { AuthContextProvider } from './contexts/AuthContext'
+import { Room } from './pages/Room';
 
 function App() {
   return (
@@ -15,8 +14,6 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
-          
-          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,11 +38,6 @@ export function Home() {
       return
     }
 
-    if(roomRef.val().endedAt) {
-      alert('Room already closed.')
-      return
-    }
-
     history.push(`/rooms/${roomCode}`);
   }
 
