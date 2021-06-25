8d4613151a8fb0e746dfb27e69d74efb9e53c723
JonnasWillian <jonnasnogueira2@gmail.com>
2021-06-25 18:26:45 -0300

    'Dia05'

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,22 +1,22 @@
-import firebase  from "firebase/app";
+import firebase from 'firebase/app';
 
 import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyASn26MlzBVlvu61nDmSkGBIXxAg1G5WGc",
-    authDomain: "letmeask-projeto-nlw.firebaseapp.com",
-    projectId: "letmeask-projeto-nlw",
-    storageBucket: "letmeask-projeto-nlw.appspot.com",
-    messagingSenderId: "301604003037",
-    appId: "1:301604003037:web:700ae98a1f1160bf9fcf34",
-    measurementId: "G-B66CRK0JP9"
-  };
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+};
 
-  firebase.initializeApp(firebaseConfig);
-  
-  const auth = firebase.auth()
-  const database = firebase.database()
-  
-  
-  export { firebase, auth, database };
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const database = firebase.database()
+
+
+export { firebase, auth, database };
